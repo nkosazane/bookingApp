7fb2af7991526cd2102f7110fa9fc73eef92e651
nkosazane <nkosazanembokazi@gmail.com>
2019-07-03 13:40:03 +0200

    bookingapp

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,13 +7,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  radio:string ;
+  drop:string;
+  drop1:string;
+  date1:number;
+  date2:number;
   constructor(private home:Router) { }
   submit()
   {
     this.home.navigateByUrl("about")
   }
+book(){
 
+  this.home.navigate(['/payment'], {queryParams:{radio: this.radio, drop: this.drop, drop1: this.drop1, date1: this.date1, date2: this.date2}})
+
+  // console.log(this.radio)
+  // console.log(this.drop)
+  // console.log(this.drop1)
+  // console.log(this.date1)
+  // console.log(this.date2)
+
+
+ // this.home.navigateByUrl("payment")
+}
   ngOnInit() {
   }
 
--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.component.scss']
 })
 export class PaymentComponent implements OnInit {
+name:string;
+cardno:number;
+cvc:number;
+expary:number;
 
-  constructor() { }
+@Input() radio:string ;
+@Input() drop:string;
+@Input() drop1:string;
+@Input() date1:number;
+@Input() date2:number;
 
+  constructor(private router:ActivatedRoute) { }
+  
   ngOnInit() {
+    this.router.queryParams.subscribe( data => {console.log(data);
+
+      this.radio = data.radio, this.drop = data.drop, this.drop1 = data.drop1, this.date1 = data.date1, this.date2 = data.date2
+      
+      console.log(this.radio, this.drop, this.drop1, this.date1, this.date2);
+      });
+      
+  }
+  pay(){
+
+    console.log(this.name)
+    console.log(this.cardno)
+    console.log(this.cvc)
+    console.log(this.expary)
+    
+    alert("you have successfully purchase your ticket");
   }
 
 }
